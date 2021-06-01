428b1071fc67461c786cd0d8f7cb4b07d9872163
mhmodo10 <mhmodarab55@gmail.com>
2021-06-01 10:10:00 +0200

    created CAR edsr model. corrected residual block

--- scripts/Model.py
+++ scripts/Model.py
@@ -9,7 +9,7 @@ from ResidualBlock import ResidualBlock
 from CustomSchedular import CustomLearningRateScheduler
 from Utils import Utils
 import matplotlib.pyplot as plt
-import keras.backend as K
+from ReflectionPadding import ReflectionPadding2D
 
 class SuperResModel:
     def __init__(self):
@@ -93,38 +93,33 @@ class SuperResModel:
     """
     def Model(self):
         conv_args = {
-            "activation": "relu",
+            "activation": LeakyReLU(),
             "kernel_initializer": "Orthogonal",
             "padding": "same",
         }
         rb = ResidualBlock()
         inputs = Input(shape=[None,None,3])
         c1 = Conv2D(64,5,input_shape = (None,None,3),**conv_args)(inputs)
-        pr1 = PReLU(alpha_initializer="zeros",shared_axes=[1,2])(c1)
 
-        resUnit = rb.ResBlock(pr1,num_of_units = 24,num_of_filters = 64)
+        resUnit = rb.ResBlock(c1,num_of_units = 32)
         
-        add1 = Add()([pr1,resUnit])
-        c2 = Conv2D(64,5,**conv_args)(add1)
+        add1 = Add()([c1,resUnit])
+        leaky = LeakyReLU()(add1)
+        c2 = Conv2D(64,5,**conv_args)(leaky)
 
         c3 = Conv2D(64,5,**conv_args)(c2)
-        pr2 = PReLU(alpha_initializer="zeros",shared_axes=[1,2])(c3)
-        c4 = Conv2D(64,5,**conv_args)(pr2)
+        c4 = Conv2D(64,5,**conv_args)(c3)
 
         c5 = Conv2D(64,5,**conv_args)(c4)
-        pr3 = PReLU(alpha_initializer="zeros",shared_axes=[1,2])(c5)
 
-        up1 = UpSampling2D(size = (2,2),interpolation = "nearest")(pr3)
+        up1 = UpSampling2D(size = (2,2),interpolation = "nearest")(c5)
         c6 = Conv2D(64,5,**conv_args)(up1)
-        leaky = LeakyReLU()(c6)
-        up2 = UpSampling2D(size = (2,2), interpolation="nearest")(leaky)
+        up2 = UpSampling2D(size = (2,2), interpolation="nearest")(c6)
         c7 = Conv2D(64,5,**conv_args)(up2)
-        leaky2 = LeakyReLU()(c7)
 
-        c8 = Conv2D(64,3,**conv_args)(leaky2)
+        c8 = Conv2D(64,3,**conv_args)(c7)
         c9 = Conv2D(64,3,**conv_args)(c8)
-        pr4 = PReLU(alpha_initializer="zeros",shared_axes=[1,2])(c9)
-        c10 = Conv2D(3,3,**conv_args)(pr4)
+        c10 = Conv2D(3,3,**conv_args)(c9)
 
         return tf.keras.Model(inputs = inputs, outputs = c10)
 
@@ -168,7 +163,69 @@ class SuperResModel:
         output = Activation("softmax")(decoded)
 
         return tf.keras.Model(inputs = inputs, outputs = output)
+    
+    def Resampler(self):
+        rb = ResidualBlock()
+        inputs = Input(shape =[None,None,3])
+        c1 = Conv2D(3, 3, padding = "same", activation = LeakyReLU())(inputs)
+        c2 = Conv2D(32,1,strides=(2,2), padding = "same")(c1)
+        #ref = ReflectionPadding2D()(c2)
+        c3 = Conv2D(128,1,strides=(2,2), padding = "same")(c2)
+        #ref = ReflectionPadding2D()(c3)
+        res_net = rb.ResBlock(c3,num_of_units=5, num_of_filters=128)
+        merged = Add()([res_net, c3])
+        lrelu = LeakyReLU()(merged)
+        #ref = ReflectionPadding2D()(lrelu)
+        c4 = Conv2D(128,1,strides=(2,2), padding = "same")(lrelu)
+        c5 = Conv2D(256, 3, padding = "same", activation = LeakyReLU())(c4)
+        c6 = Conv2D(256, 3, padding = "same", activation = LeakyReLU())(c5)
+        c7 = Conv2D(256, 3, padding = "same", activation = LeakyReLU())(c6)
+        upsample = UpSampling2D()(c7)
+        c3 = Conv2D(3, 1,padding = "same")(upsample)
+        model = tf.keras.Model(inputs = inputs, outputs = c3)
+        #model.summary()
+        return model
+    
+    def EDSR(self):
+        rb = ResidualBlock()
+        inputs = Input(shape=[None,None,3])
+        c1 = Conv2D(256, 3, padding = "same", activation = LeakyReLU())(inputs)
+        res_block = rb.ResBlock(c1,num_of_units = 32, num_of_filters = 256)
+        merged = Add()([res_block, c1])
+        lrelu = LeakyReLU()(merged)
+        up = UpSampling2D()(lrelu)
+        c2 = Conv2D(128, 1,padding = "same")(up)
+        up = UpSampling2D()(c2)
+        c3 = Conv2D(3, 1,padding = "same")(up)
+        lrelu = LeakyReLU()(c3)
+        model = tf.keras.Model(inputs = inputs, outputs = lrelu)
+        #model.summary()
+        return model
 
+    def TrainCAR(self,model = None,
+    X_train_path = None , y_train_path = None,
+    num_of_epochs = 100, checkpoint_filepath = None,
+    existing_weights = None, load_weights = False,
+    optimizer = None,logsdir = None, **args):
+        tensorboard = TensorBoard(log_dir = logsdir)
+        early_stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        save_weights_only=False,
+        monitor='loss',
+        mode='min',
+        save_best_only=False)
+        if load_weights and not existing_weights == None:
+            model.set_weights(existing_weights)
+        model.summary()
+        model.compile(optimizer = optimizer, loss = self.L1Loss)
+        data = self.Utils.LoadH5File(X_train_path)/255
+        model.fit(data,
+        data,
+        batch_size = 5,
+        validation_split = 0.1,
+        epochs= num_of_epochs,
+        callbacks=[tensorboard, model_checkpoint_callback])
 
     """drops learning rate by 50% each 20 epochs
 
@@ -286,17 +343,23 @@ class SuperResModel:
         return count * 20
 
 model = SuperResModel()
+optim_args = {
+    "learning_rate" : 1e-4,
+    "beta_1" : 0.9,
+    "beta_2" : 0.999,
+    "epsilon" : 1e-6
+}
 model_args = {
-    "model" : model.DenoisingModel(),
-    "X_train_path" : r"D:\HBO\MinorAi\PickleFiles\train_lr_bicubic.h5",
+    "model" : tf.keras.Sequential([model.Resampler(), model.EDSR()], name= "CAR_EDSR"),
+    "X_train_path" : r"D:\HBO\MinorAi\PickleFiles\train_lr.h5",
     "y_train_path" : r"D:\HBO\MinorAi\PickleFiles\train_lr.h5",
     "num_of_epochs" : 100,
-    "logsdir" : "denoiser_23_5_21",
-    "checkpoint_filepath" : "denoiser",
+    "logsdir" : "car_edsr_31_5",
+    "checkpoint_filepath" : "super_res_car_edsr",
     "existing_weights" : None,
     "load_weights" : False,
-    "optimizer" : "adam",
-
+    "optimizer" : tf.keras.optimizers.Adam(**optim_args),
 }
-model.PredictAndShowImage(model.loadModel('super_res.h5'), data_path=r"D:\HBO\MinorAi\test", read_from_directory = True)
+#model.PredictAndShowImage(model.loadModel('super_res_car_edsr').layers[1], data_path=r"D:\HBO\MinorAi\test", read_from_directory = True)
 #model.TrainModel(**model_args)
+model.TrainCAR(**model_args)
--- scripts/ResidualBlock.py
+++ scripts/ResidualBlock.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D,PReLU,Add
+from tensorflow.keras.layers import Conv2D,PReLU,Add,Input,LeakyReLU
 from tensorflow.keras.initializers import Constant
 class ResidualBlock:
     def __init__(self):
@@ -15,15 +15,22 @@ class ResidualBlock:
     Returns:
         (keras layer) : the last layer of the residual block
     """
-    def ResBlock(self,input_data,num_of_units = 3, num_of_filters = 64, kernel_size = (3,3)):
-
-        resunit = []
+    def ResBlock(self,input_data,num_of_units = 3, num_of_filters = 16, kernel_size = (3,3), inc_filters = False):
+        
+        res_unit = input_data
+        block_size = num_of_units // 3
         for i in range(num_of_units):
+            res_input = res_unit
             if i == 0:
-                resUnit = self.ResidualUnit(input_data,num_of_filters, kernel_size = kernel_size)
+                res_unit = self.ResidualUnit(input_data,num_of_filters, kernel_size = kernel_size)
             else:
-                resUnit = self.ResidualUnit(resUnit, num_of_filters, kernel_size = kernel_size)
-        return resUnit
+                res_unit = self.ResidualUnit(res_unit, num_of_filters, kernel_size = kernel_size)
+                res_unit = Add()([res_input, res_unit])
+                res_unit = LeakyReLU()(res_unit)
+            if i % block_size == 0 and inc_filters:
+                num_of_filters *= 2
+        
+        return res_unit
 
 
     """Residual unit that consists of 2 Conv2D layers and one PReLU layer
@@ -36,9 +43,7 @@ class ResidualBlock:
         (keras layer) : result of the last layer in the unit
     """
     def ResidualUnit(self,input_data, num_of_filters, kernel_size = (3,3)):
-        identity = input_data
-        x = Conv2D(num_of_filters,kernel_size,padding="same")(identity)
-        x = PReLU(alpha_initializer=Constant(value=0.25),shared_axes=[1,2])(x)
+        x = Conv2D(num_of_filters,kernel_size,padding="same")(input_data)
+        x = LeakyReLU()(x)
         x = Conv2D(num_of_filters,kernel_size,padding = "same")(x)
-        x = PReLU(alpha_initializer=Constant(value=0.25),shared_axes=[1,2])(x)
         return x
